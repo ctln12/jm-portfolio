2e5a763e2ba5e29639b1e77fe2444c1f656f4ff4
Cataline <catalinebaert@yahoo.com>
2021-06-15 16:57:03 +0200

    Create global style, install Roboto font and set it as default, create components folder and a Container component, create style page for components, modify homepage

---
+++ gatsby-browser.js
@@ -0,0 +1,5 @@
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./src/styles/global.css";
